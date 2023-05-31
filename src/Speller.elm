f0c09b0dbb3c23945e5b6907eb1eb3e76aa52e1e
Matt Savoia <matt@1-800-rad-dude.com>
2023-05-30 22:14:50 -0400

    Add formatTimeRemaining

--- src/Speller.elm
+++ src/Speller.elm
@@ -439,11 +439,18 @@ scoreView score =
     h2 [] [ text ("Score: " ++ String.fromInt score) ]
 
 
+formatTimeRemaining : Int -> String
+formatTimeRemaining remainingTime =
+    remainingTime
+        |> String.fromInt
+        |> String.padLeft 2 '0'
+
+
 timerView : Maybe Time.Posix -> Maybe Time.Posix -> Html Msg
 timerView startTime currentTime =
     case ( startTime, currentTime ) of
         ( Just startTimestamp, Just currentTimestamp ) ->
-            h2 [] [ text ("Time: " ++ String.fromInt (timeRemaining startTimestamp currentTimestamp)) ]
+            h2 [] [ text ("Time: " ++ formatTimeRemaining (timeRemaining startTimestamp currentTimestamp)) ]
 
         ( _, _ ) ->
             text ""
